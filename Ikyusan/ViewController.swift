abe370c26176c595a0e1b6531297c5ec77999692
ushisantoasobu <babblemann.shunsee@gmail.com>
2015-04-29 11:32:46 +0900

    first commit

---
+++ Ikyusan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ikyusan
+//
+//  Created by SatoShunsuke on 2015/04/29.
+//  Copyright (c) 2015年 moguraproject. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
