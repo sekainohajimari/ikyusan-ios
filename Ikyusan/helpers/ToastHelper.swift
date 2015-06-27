a20f685b6776c154c2ba5e9f865d7153405e3a18
ushisantoasobu <babblemann.shunsee@gmail.com>
2015-06-27 13:11:00 +0900

    create toasthelper

---
+++ Ikyusan/helpers/ToastHelper.swift
@@ -0,0 +1,17 @@
+//
+//  ToastHelper.swift
+//  Ikyusan
+//
+//  Created by SatoShunsuke on 2015/06/27.
+//  Copyright (c) 2015年 moguraproject. All rights reserved.
+//
+
+import UIKit
+
+class ToastHelper: NSObject {
+
+    class func make(view :UIView, message :String) {
+        view.makeToast(message, duration: 0.75, position: nil)
+    }
+
+}
