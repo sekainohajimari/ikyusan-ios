66f7504bfb0024a8102831efa8a061d8a5c686ba
ushisantoasobu <babblemann.shunsee@gmail.com>
2015-07-19 16:35:51 +0900

    modify notification model

--- Ikyusan/models/Notification.swift
+++ Ikyusan/models/Notification.swift
@@ -3,41 +3,20 @@ import ObjectMapper
 import Bond
 
 class Notification: Mappable {
-    /*
-    #  id                    :integer          not null, primary key
-    #  notifier_id           :integer
-    #  type                  :string(255)
-    #  notificationable_type :string(255)
-    #  notificationable_id   :string(255)
-    #  notification_kind     :integer
-    #  progress              :integer
-    #  created_at            :datetime         not null
-    #  updated_at            :datetime         not null
-*/
-    
-    var identifier              = Dynamic<Int>(0)
-    var notifierId              = Dynamic<Int>(0)
-    var type                    = Dynamic<String>("")
-    var notificationableType    = Dynamic<String>("")
-    var notificationableId      = Dynamic<String>("")
-    var notificationKind        = Dynamic<Int>(0)
-    var progress                = Dynamic<Int>(0)
-    var createdAt               = Dynamic<String>("")
-    var updatedAt               = Dynamic<String>("")
-    
+
+    var identifier          = Dynamic<Int>(0)
+    var title               = Dynamic<String>("")
+    var body                = Dynamic<String>("")
+    var opened              = Dynamic<Bool>(false)
+
     required init?(_ map: Map) {
         mapping(map)
     }
     
     func mapping(map: Map) {
-        identifier              <- map["id"]
-        notifierId              <- map["notifier_id"]
-        type                    <- map["type"]
-        notificationableType    <- map["notificationable_type"]
-        notificationableId      <- map["notificationable_id"]
-        notificationKind        <- map["notification_kind"]
-        progress                <- map["progress"]
-        createdAt               <- map["created_at"]
-        updatedAt               <- map["updated_at"]
+        identifier      <- map["id"]
+        title           <- map["title"]
+        body            <- map["body"]
+        opened          <- map["opened"]
     }
 }
